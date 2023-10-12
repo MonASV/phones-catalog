dfd8a6a162e3dfa540d45ab9d40ee9bca3d6e24b
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-10-12 13:27:57 +0200

    Iteration 1: REST API

---
+++ phone-catalog-server/routes/index.routes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const Phone = require("../models/User.model");
+
+router.get("/phones", (req, res, next) => {
+  Phone.find()
+    .then((phones) => {
+      res.json(phones)
+    console.log(phones);})
+    .catch((error) => {
+      console.log("Error getting the list of phones", error);
+      res.status(500).json({
+        message: "we are sorry, we coudn't dislpay the phone catalog",
+      });
+    });
+});
+
+router.get("/phones/:id", (req, res, next) => {
+  const { phoneId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(phoneId)) {
+    res.status(400).json({
+      message: "Phone ID not valid",
+    });
+    return;
+  }
+
+  Phone.findById(phoneId)
+    .then((phone) => res.status(200).json(phone))
+    .catch((error) => {
+      console.log("Error getting the desired phone", error);
+      res.status(500).json({
+        message: "we are sorry, we coudn't find your desired phone",
+      });
+    });
+});
+
+module.exports = router;
